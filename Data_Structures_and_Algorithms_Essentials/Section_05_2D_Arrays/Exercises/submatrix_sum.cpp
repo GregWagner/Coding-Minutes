d54d51a9b707839cfed1253728681c8b9ee63c13
Greg Wagner <370026+GregWagner@users.noreply.github.com>
2022-04-26 09:11:39 -0500

    Syncing with casey

--- Data_Structures_and_Algorithms_Essentials/Section_05_2D_Arrays/Exercises/submatrix_sum.cpp
+++ Data_Structures_and_Algorithms_Essentials/Section_05_2D_Arrays/Exercises/submatrix_sum.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 
-int main() {
+int sum(std::vector<std::vector<int>> v, int sr, int sc, int er, int ec) {
+  std::vector<std::vector<int>> answer;
 
+  return 0;
 }
+
+int main() {}
