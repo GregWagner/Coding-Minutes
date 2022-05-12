45f16a22968cd4095168f7c8ff1d668a23e5f6db
Greg Wagner <theGreg101@gmail.com>
2022-05-12 05:49:40 -0500

    From new linux

--- Data_Structures_and_Algorithms_Essentials/Section_08_Bit_Manipulation/Exercises/xoring.cpp
+++ Data_Structures_and_Algorithms_Essentials/Section_08_Bit_Manipulation/Exercises/xoring.cpp
@@ -3,19 +3,22 @@
  * present twice except for one number which is only presennt
  * once. Find the unique number without taking any extra space
  * and in linear time.
+ *
+ * XOR all the values and the resulting value will be the
+ * sole value
  */
 #include <iostream>
 #include <vector>
 
 int xoring(std::vector<int> v) {
-    int answer {};
-    for (const auto &ele : v) {
-        answer ^= ele;
-    }
-    return answer;
+  int answer{};
+  for (const auto &ele : v) {
+    answer ^= ele;
+  }
+  return answer;
 }
 
 int main() {
-    std::vector<int> v {1, 2, 3, 1, 4, 2, 3};
-    std::cout << xoring(v) << '\n';
+  std::vector<int> v{1, 2, 3, 1, 4, 2, 3};
+  std::cout << xoring(v) << '\n';
 }
