88c141b6b6bcf5c74b1f325f92e4d64e139845fe
Greg Wagner <370026+GregWagner@users.noreply.github.com>
2022-05-15 10:09:23 -0500

    Updated Section 12

---
+++ Data_Structures_and_Algorithms_Essentials/Section_12_Backtracking/Examples/fill_vector.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+void printVector(const std::vector<int> &a, int n) {
+    for (int i{}; i < n; ++i) {
+        std::cout << a[i] << ' ';
+    }
+    std::cout << '\n';
+}
+
+// note a is shared accross all function calls
+void fillVector(std::vector<int> &a, int index, int n, int value) {
+    // base case
+    if (index == n) {
+        printVector(a, 5);
+        return;
+    }
+
+    // recursive case
+    a[index] = value;
+    fillVector(a, index + 1, n, value + 1);
+
+    // backtracking step (always after the recursive function call)
+    a[index] = -1 * a[index];
+}
+
+int main() {
+    std::vector<int> a(100);
+    fillVector(a, 0, 5, 1);
+    printVector(a, 5);
+}
---
+++ Data_Structures_and_Algorithms_Essentials/Section_12_Backtracking/Examples/grid_ways.cpp
@@ -0,0 +1,30 @@
+/*
+ * Given an MxN grid, find the number of
+ * ways to reach the bottom right corner
+ * starting from the top left. At each cell
+ * you can move right or down
+ *
+ * At each cell, there are 2 sub-problems.
+ * Going right and going down.
+ * f(x, y) = f(x + 1, y) + f(x, y + 1)
+ *
+ * Note: There is a math trick to this. Calculate
+ * the number of permutations of m-1 + n-1 moves.
+ *    (m + n - 2)!
+ * -----------------
+ *  (m - 1)!(n - 1)!
+ */
+#include <iostream>
+
+int gridWays(int i, int j, int m, int n) {
+    // base case - at the bottom right
+    if (i == m - 1 && j == n - 1) {
+        return 1;
+    }
+    if (i >= m || j >= m) {
+        return 0;
+    }
+    return gridWays(i + 1, j, n, m) + gridWays(i, j + 1, n, m);
+}
+
+int main() { std::cout << gridWays(0, 0, 3, 3) << '\n'; }
